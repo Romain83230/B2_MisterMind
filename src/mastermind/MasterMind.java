041dd72fdf7c583e045f2daab469560c5965442d
Lorenzo Chaudessolle <l.chaudessolle@gmail.com>
2017-12-19 16:37:28 +0100

    base de données / login

--- src/mastermind/MasterMind.java
+++ src/mastermind/MasterMind.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package mastermind;
+import java.sql.SQLException;
+import java.util.Scanner;
 
 /**
  *
@@ -14,8 +16,24 @@ public class MasterMind {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
         // TODO code application logic here
+        /*System.out.println("Veuillez saisir un mot :");
+        String entree = sc.nextLine();
+        System.out.println("Vous avez saisi : " + entree);*/
+        DBConnection dbco = new DBConnection();
+        dbco.Initialize();
+        System.out.println("Veuillez saisir votre login :");
+        String login = sc.nextLine();
+        System.out.println("Veuillez saisir votre mot de passe :");
+        String password = sc.nextLine();
+        while(!dbco.VerifConnection(login, password)){
+            System.out.println("Veuillez saisir votre login :");
+            login = sc.nextLine();
+            System.out.println("Veuillez saisir votre mot de passe :");
+            password = sc.nextLine();
+        }
     }
     
 }
