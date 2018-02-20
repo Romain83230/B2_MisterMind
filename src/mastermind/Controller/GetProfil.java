a9b3a0ef61465d7f0af0e2e6579a8b538fa977f1
lchaudessolle <l.chaudessolle@gmail.com>
2018-02-20 15:50:14 +0100

    Add files via upload

--- src/mastermind/Controller/GetProfil.java
+++ src/mastermind/Controller/GetProfil.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package mastermind.Controller;
 import mastermind.View.*;
 
@@ -7,27 +12,21 @@ import mastermind.View.*;
  */
 public class GetProfil extends AbstractController{
 
-    /**
-     * Constructeur manipulant les informations de la classe parente grâce à la méthode super().
-     * @param nom le nom de l'utilisateur.
-     * @param auth bolléen, true si l'utilisateur est authentifié, sinon false.
-     */
     public GetProfil(String nom, boolean auth) {
         super(nom, auth);
     }
-    
     @Override
     public void perform() {
         this.setView(new ProfilView(this));
-        this.getView().send("Login : " + Database.Select("login", this.getName()));
-        this.getView().send("Nom : " + Database.Select("nom", this.getName()));
-        this.getView().send("Prénom : " + Database.Select("prenom", this.getName()));
-        this.getView().send("Email : " + Database.Select("email", this.getName()));
-        this.getView().send("Appuyez sur 'entrée' pour continuer.");
-        input.nextLine();
+        this.getView().send("Login : " + Database.Select("login", "joueur", "login", this.getName()));
+        this.getView().send("Nom : " + Database.Select("nom", "joueur", "login", this.getName()));
+        this.getView().send("Prénom : " + Database.Select("prenom", "joueur", "login", this.getName()));
+        this.getView().send("Email : " + Database.Select("email", "joueur", "login", this.getName()));
+        this.getView().send("Date de naissance : " + Database.Select("date_naissance", "joueur", "login", this.getName()));
+        
     }
     
     public String getMail(){
-        return Database.Select("email", this.getName());
+        return Database.Select("email", "joueur", "login", this.getName());
     }
 }
